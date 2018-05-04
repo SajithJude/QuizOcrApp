022d09eae4dd34ae896ec43b5116fa5e00402883
KeianBarton <Keian.Barton@civica.co.uk>
2018-05-04 22:08:54 +0100

    Google search works

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -11,37 +12,50 @@ export class QuestionComponent implements OnInit {
   submitted : boolean = false;
 
   @Input() question : string;
-  @Input() answers : string[] = new Array<string>();
-  googlePages : string[] = new Array<string>();
-  likelihood : number[] = new Array<number>();
+  @Input() answers : string[] = ["","",""];
+  googlePages : string[] = ["","",""];
+  bingPages : string[] = ["","",""];
+  answerCounts : number[] = [0,0,0];
+  likelihood : string[] = ["","",""];
 
-  constructor(private http: Http) { }
+  constructor(private router: Router, private http: Http) { }
 
   ngOnInit() {
   }
 
   handleSubmit() {
-    this.submitted = true;
-    for(var i = 0; i < this.answers.length - 1; i++)
-    {
-      this.http.get(
+    var self = this;
+    self.submitted = true;
+    self.answers.forEach(function(answer, i) {
+      self.http.get(
         "https://www.googleapis.com/customsearch/v1?q=" +
-        this.question.replace(/[^\w\s]/gi, '').toLowerCase().split(" ").join("+") +
-        this.answers[i].replace(/[^\w\s]/gi, '').toLowerCase().split(" ").join("+") +
+        self.question.replace(/[^\w\s]/gi, '').toLowerCase().split(" ").join("+") +
+        self.answers[i].replace(/[^\w\s]/gi, '').toLowerCase().split(" ").join("+") +
         "&num=10&c2coff=1&filter=0&lr=lang_en&" +
         "key=AIzaSyAnbHJbJR8zdaDhG9pigGjtU3SKLHjFHqU&" +
         "cx=009790495881824375879:so76g6onpgu"
         )
         .subscribe(
           (res: any) => {
-            this.googlePages[i] = JSON.stringify(res).toLowerCase();
-            this.likelihood[i] = this.countOcurrences(this.googlePages[i],this.answers[i]);
+            self.googlePages[i] = JSON.stringify(res).toLowerCase();
+            self.answerCounts[i] = self.countOcurrences(self.googlePages[i],self.answers[i]);
+            if(i == self.answers.length - 1)
+            {
+              self.answerCounts.forEach(function(answer, j) {
+                self.likelihood[j] = Math.round(
+                  (self.answerCounts[j] / self.sum(self.answerCounts)) * 100) + '%';
+              });
+            }
           },
-          (err: Error) => this.handleError
+          (err: Error) => self.handleError
         );
-    }
+    });
   }
 
+  returnToCamera() {
+    this.router.navigate(['camera']);
+  }
+  
   private handleError(error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
@@ -57,6 +71,16 @@ export class QuestionComponent implements OnInit {
   private countOcurrences(str, value) {
     var regExp = new RegExp(value, "gi");
     return (str.match(regExp) || []).length;
+  }
+
+  private isAnswerLoaded(num : number) {
+    return this.likelihood[num] != "";
+  }
+
+  private sum(numbers : number[]) {
+    return numbers.reduce(function(a,b) {
+      return a + b;
+    });
   }
 
 }
