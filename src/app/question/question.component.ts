b484f7127f90b9fb8f9b88efaf0161d28aaded72
KeianBarton <Keian.Barton@civica.co.uk>
2018-05-08 13:18:04 +0100

    Text snippets shown on screen

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/observable/forkJoin'
 
 @Component({
   selector: 'app-question',
@@ -12,7 +15,7 @@ export class QuestionComponent implements OnInit {
   ocrText : string;
   question : string;
   answers : string[] = ["","",""];
-  googlePages : string[] = ["","",""];
+  snippets : string[] = ["","",""];
   answerCounts : number[] = [0,0,0];
   likelihood : string[] = ["","",""];
 
@@ -27,46 +30,74 @@ export class QuestionComponent implements OnInit {
     var questionStartIdx = this.ocrText.search("\\nH") + 4;
     var questionEndIdx = this.ocrText.search("\\?\\n") + 1;
     var answersEndIdx = this.ocrText.search("\\nSwipe") + 1;
-    this.question = this.ocrText.substring(questionStartIdx, questionEndIdx);
-    this.answers = this.ocrText.substring(questionEndIdx + 1, answersEndIdx - 1).split(/\n/);
-    this.commenceGoogleSearch();
+    this.question = `What colour is the
+    Incredible Hulk on his very
+    first comic book cover?`//this.ocrText.substring(questionStartIdx, questionEndIdx);
+    this.answers = ["Green", "Grey", "Red"];//this.ocrText.substring(questionEndIdx + 1, answersEndIdx - 1).split(/\n/);
+    //this.commenceGoogleSearch();
   }
 
   commenceGoogleSearch() {
     var self = this;
-    self.answers.forEach(function(answer, i) {
-      self.http.get(
-        "https://www.googleapis.com/customsearch/v1?q=" +
-        self.question.replace(/[^\w\s]/gi, '').toLowerCase().replace(/(\r\n\t|\n|\r\t)/gm," ").split(" ").join("+") + " " +
-        self.answers[i].replace(/[^\w\s]/gi, '').toLowerCase().replace(/(\r\n\t|\n|\r\t)/gm," ").split(" ").join("+") +
-        "&num=10&c2coff=1&filter=0&lr=lang_en&" +
-        "key=AIzaSyAnbHJbJR8zdaDhG9pigGjtU3SKLHjFHqU&" +
-        "cx=009790495881824375879:so76g6onpgu"
-        )
-        .subscribe(
-          (res: any) => {
-            self.googlePages[i] = res.json().items.map(i => i.snippet).join(" ").toLowerCase();
-            self.answerCounts[i] = self.countOcurrences(self.googlePages[i],self.answers[i]);
-            if(i == self.answers.length - 1)
-            {
-              self.answerCounts.forEach(function(answer, j) {
-                self.likelihood[j] = Math.round(
-                  (self.answerCounts[j] / self.sum(self.answerCounts)) * 100) + '%';
-              });
-            }
-          },
-          (err: Error) => {
-            self.handleError(err);
-            self.likelihood.forEach(function(l){
-              l = "?";
-            });
+    Observable.forkJoin(
+      self.searchForAnswer(0),
+      self.searchForAnswer(1),
+      self.searchForAnswer(2)
+    )
+    .subscribe(
+      (res: any) => {
+        self.answerCounts.forEach(function(answer, j) {
+          if(answer != -1)
+          {
+            self.likelihood[j] = Math.round(
+              (self.answerCounts[j] / self.sum(self.answerCounts)) * 100) + '%';
           }
-        );
-    });
+          else
+          {
+            self.likelihood[j] = "?";
+          }
+        })
+      }
+    );
   }
 
   returnToCamera() {
     this.router.navigate(['camera']);
+  }
+
+  private searchForAnswer(i : number) {
+    var self = this;
+    return self.http.get(
+      "https://www.googleapis.com/customsearch/v1?q=" +
+      self.question.replace(/[^\w\s]/gi, '').toLowerCase().replace(/(\r\n\t|\n|\r\t)/gm," ").split(" ").join("+") + " " +
+      self.answers[i].replace(/[^\w\s]/gi, '').toLowerCase().replace(/(\r\n\t|\n|\r\t)/gm," ").split(" ").join("+") +
+      "&num=10&c2coff=1&filter=0&lr=lang_en&" +
+      "key=AIzaSyAnbHJbJR8zdaDhG9pigGjtU3SKLHjFHqU&" +
+      "cx=009790495881824375879:so76g6onpgu"
+      )
+    .map(
+      (res: any) => {
+        if (res.json().items)
+        {
+          self.snippets[i] = res.json().items.map(i => i.snippet).join("\n\n").toLowerCase();
+          var regExp = new RegExp("\\b" + self.answers[i] +"\\b", "gi");
+          self.snippets[i] = self.snippets[i].replace(regExp, `<span class="match">$&</span>`);
+          self.answerCounts[i] = self.countOcurrences(self.snippets[i],self.answers[i]);
+        }
+        else
+        {
+          self.snippets[i] = "";
+          self.answerCounts[i] = -1;
+        }
+        return res.json();
+      },
+      (err: Error) => {
+        self.handleError(err);
+        self.likelihood.forEach(function(l){
+          l = "?";
+        });
+      }
+    );
   }
   
   private handleError(error: Response | any) {
